c5963a3c091d09381cd6db753e2fb66724d41c70
Haran Shivanan <shivanan@statictype.org>
2012-09-06 22:28:07 +0530

    Added flask app

--- hnusers.py
+++ hnusers.py
@@ -1,6 +1,24 @@
+import sys
+import os
+from flask import Flask,render_template,request,session,redirect,url_for
 import hn
 # for user in ['patio11','tptacek','anigbrowl','pg','raganwald']:
 # 	hn.updateuser(user)
-for x in hn.latestcomments(['patio11','tptacek']):
-	print x['comment']
-	print ' '
+
+# for x in hn.latestcomments(['patio11','tptacek']):
+# 	print x['comment']
+# 	print ' '
+
+app = Flask(__name__)
+
+@app.route('/threads')
+def threads():
+	if not 'id' in request.args:
+		return 'nope'
+	ids = request.args['id']
+	print ids
+	return '|'.join(ids.split(','))
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
